bc7a3f0f8a5cef5ca45c5fd879daac359351664e
matthewcumbo <matthew.abraham.cumbo@mcast.edu.mt>
2026-04-13 12:41:36 +0200

    Introduced Response Codes

--- api/user/delete.php
+++ api/user/delete.php
@@ -6,6 +6,11 @@ header("Access-Control-Allow-Methods: DELETE");
 
 header("Access-Control-Allow-Headers: Access-Control-Allow-Origin, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With");
 
+if($_SERVER["REQUEST_METHOD"] != "DELETE"){
+    http_response_code(405);
+    echo json_encode(array("message" => "Incorrect Request Method used."));
+    die();
+}
 
 include_once("../../includes/initialize.php");
 
@@ -13,11 +18,26 @@ include_once("../../includes/initialize.php");
 // This allows us to use its structure and functions
 $user = new User($db);
 
-$user->id = isset($_GET["id"]) ? $_GET["id"] : die();
+// $user->id = isset($_GET["id"]) ? $_GET["id"] : die();
+
+
+// Check if ID is provided in query string
+if(isset($_GET["id"])){
+    $user->id = $_GET["id"];
+}
+else{
+    // if not, Error 401 (Bad Request)
+    http_response_code(401);
+    echo json_encode(array("message" => "User ID was not provided."));
+    die();
+}
+
 
 if($user->delete()){
+    http_response_code(200);
     echo json_encode(array("message" => "User deleted."));
 }
 else{
+    http_response_code(500);
     echo json_encode(array("message" => "User not deleted."));
 }
